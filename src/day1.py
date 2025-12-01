4a079f512db4ff33659ee79afe8b3ae718eaf903
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2025-12-01 18:50:52 +0800

    Golf day1

--- src/day1.py
+++ src/day1.py
@@ -1,9 +1,9 @@
 with open("res/day1.txt") as f:
-    inp = f.read().split('\n')
+    s = f.read().split('\n')
 
 curr = 50
 count1, count2 = 0, 0
-for i, dir in enumerate(inp):
+for dir in s:
     rot = int(dir[1:])
     if dir[0] == 'L': curr = (100 - curr) % 100
     d, curr = divmod(curr + rot, 100)
@@ -13,3 +13,10 @@ for i, dir in enumerate(inp):
     
 print("Part 1:", count1)
 print("Part 2:", count2)
+
+# Golf - 129 chars
+d,r,o,t,h=50,'R',0,0,100
+print(*map(sum,zip(*[(t+(d:=(d if r==(r:=i[0]) else (h-d)%h)+int(i[1:]))//h,o+(d:=d%h)==0)for i in s])))
+
+# That prints it as 'part2 part1', to print part1 first costs an extra 7 characters
+print(*map(sum,zip(*[(o+(d:=(d if r==(r:=i[0]) else (h-d)%h)+int(i[1:]))%h==0,t+(d,d:=d%h)[0]//h)for i in s])))
