c4ca904598d03bf94fda15f7d6199537ecb30e35
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2025-12-11 15:44:10 +0800

    Day 10 part 2

--- src/day10.py
+++ src/day10.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+from ortools.linear_solver import pywraplp
+import numpy as np
 
 with open('res/day10.txt') as f:
     lines = f.read().splitlines()
@@ -30,3 +32,27 @@ for machine in machines:
         paths = next
         total += 1
 print(f"Part 1: {total}")
+
+total = 0
+for machine in machines:
+    A = []
+    for button in machine.schematics:
+        row = [0 for _ in machine.joltage_reqs]
+        for b in button: row[b] = 1
+        A.append(row)
+    
+    A = np.array(A)
+    b = np.array(machine.joltage_reqs)
+
+    m, n = A.shape
+
+    solver = pywraplp.Solver.CreateSolver("CBC")
+    x = [solver.IntVar(0, solver.infinity(), f"x[{i}]") for i in range(m)]
+    for row in range(n):
+        solver.Add(sum(A[col, row] * x[col] for col in range(m)) == b[row])
+    solver.Minimize(solver.Sum(x))
+
+    solver.Solve()
+
+    total += sum([int(v.solution_value()) for v in x])
+print(f"Part 2: {total}")
